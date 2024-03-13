d91067c2eda715636c2acf247b1f0d7cd32be53c
kavita - Singh <135163221+Kavitkashingh@users.noreply.github.com>
2024-03-13 13:34:30 +0530

    Update server.js
    production add

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = socket(server, {
     cors: {
         origin: process.env.mode === 'pro' ? [process.env.
             client_customer_production_url, process.env.
-            client_cadmin_production_url,] : ['http://localhost:3000', 'http://localhost:3001'],
+            client_admin_production_url,] : ['http://localhost:3000', 'http://localhost:3001'],
         credentials: true
     }
 })
